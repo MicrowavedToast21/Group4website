a4d0612c0587264a94f2d3753515063f403c25b7
Lea <116588893+MicrowavedToast21@users.noreply.github.com>
2024-06-17 13:10:06 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+
+    function showSection(sectionId) {
+    // Get all sections
+    const sections = document.querySelectorAll('.section');
+    // Hide all sections
+    sections.forEach(section => {
+    section.classList.add('hidden');
+});
+    // Show the target section
+    const targetSection = document.getElementById(sectionId);
+    targetSection.classList.remove('hidden');
+}
+
